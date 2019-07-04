da33365b00680d711845a196fa7c7c70db6ee48a
Deven Phillips <deven.phillips@gmail.com>
2019-07-04 12:35:54 -0400

    Fix selectors

--- tests/e2e/specs/checkin.spec.js
+++ tests/e2e/specs/checkin.spec.js
@@ -3,6 +3,10 @@ const SW_GIVEN_NAME = process.env.SW_GIVEN_NAME;
 const SW_RES_CODE = process.env.SW_RES_CODE;
 const SW_PHONE_NUM = process.env.SW_PHONE_NUM;
 
+/*
+#swa-content > div > div:nth-child(2) > div > section > div > div > div.air-check-in-review-results--confirmation > button
+*/
+
 module.exports = {
   'Check in to Southwest Flight' : function (browser) {
     browser
@@ -12,10 +16,10 @@ module.exports = {
       .setValue('input[id=passengerFirstName]', SW_GIVEN_NAME)
       .setValue('input[id=passengerLastName]', SW_FAMILY_NAME)
       .click('button[id=form-mixin--submit-button]')
-      .waitForElementVisible('button')
-      .click('button[class=air-check-in-review-results--check-in-button]')
-      .waitForElementVisible('button')
-      .click('button[class=boarding-pass-options--button-text]')
+      .waitForElementVisible('button[data-a="airCheckInReviewResults_checkIn"]', 50000)
+      .click('button[data-a="airCheckInReviewResults_checkIn"]')
+      .waitForElementVisible('button[data-a="airCheckInBoardingPassOption_button_mobileBoardingPassOption_text"]', 50000)
+      .click('button[data-a="airCheckInBoardingPassOption_button_mobileBoardingPassOption_text"]')
       .setValue('input[id=textBoardingPass]', SW_PHONE_NUM)
       .click('button[id=form-mixin--submit-button]')
       .end();
